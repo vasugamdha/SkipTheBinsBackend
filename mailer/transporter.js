58e6196f2eaaf9430f820cab04ccd98c104164a7
unknown <vs527199@dal.ca>
2022-03-29 12:03:51 -0300

    minor fixes

--- mailer/transporter.js
+++ mailer/transporter.js
@@ -1,11 +1,11 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: 'skipthebins@gmail.com',
-        pass: 'skipthebins@123'
-    }
+  service: "gmail",
+  auth: {
+    user: "skipthebins@gmail.com",
+    pass: "skipthebins@123",
+  },
 });
 
-module.exports = {transporter}
+module.exports = transporter;
