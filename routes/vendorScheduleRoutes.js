d6f9a745c28e9780b18efe68d917b4bec6aeda91
Prashit Prakashbhai Patel <prashitppatel@gmail.com>
2022-04-08 23:58:12 -0300

    code comments

--- routes/vendorScheduleRoutes.js
+++ routes/vendorScheduleRoutes.js
@@ -5,16 +5,22 @@ const router = express.Router();
 const vendorScheduleController = require("../controllers/vendorScheduleController");
 const auth = require("../middlewares/authMiddleware.js");
 
+//route to get schedules
 router.get("/schedules", auth, vendorScheduleController.getSchedule);
 
+//route to create schedules
 router.post("/create", auth, vendorScheduleController.createSchedule);
 
+//route to update schedule
 router.put("/update/:id", auth, vendorScheduleController.updateSchedule);
 
+//route to delete schedule
 router.delete("/delete/:id", auth, vendorScheduleController.deleteSchedule);
 
+//route to update status of individual schedule
 router.put("/schedules/update", auth, vendorScheduleController.updateStatus);
 
+//route ro add new individual schedule
 router.post("/schedules/add", auth, vendorScheduleController.addSchedule);
 
 module.exports = router;
